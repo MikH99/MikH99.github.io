19035b95dcec83b81a6c74442270d5b2ffacf3f8
MikH99 <ma15290640603@163.com>
2022-07-29 22:56:06 +0800

    添加鼠标点击特效

--- js/about.js
+++ js/about.js
@@ -408,6 +408,7 @@ function timer() {
         return t - time;
     };
 };
+//页面飘雪花
  (function($){
      $.fn.snow = function(options){
      var $flake = $('<div id="snowbox" />').css({'position': 'absolute','z-index':'9999', 'top': '-50px', 'cursor': 'pointer'}).html('❄'),
@@ -449,5 +450,35 @@ function timer() {
          newOn: 200  /* 定义密集程度，数字越小越密集 */
      });
  });
+//鼠标点击出现字
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array("❤郑恩地❤","❤李知恩❤","❤金泰妍❤","❤希望天空❤","❤除了春天、爱情和樱花❤","❤why❤","❤少女的少年❤","❤palette❤","❤rain❤","❤夜信❤","❤blueming❤","❤离别初体验❤");
+        var $i = $("<span></span>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+        3000,
+        function() {
+            $i.remove();
+        });
+    });
+});
+
+
 
 about_main()
