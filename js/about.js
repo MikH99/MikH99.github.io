0fdb2bc9b04d22cf8e8fc7b6017733446b7e1983
MikH99 <ma15290640603@163.com>
2022-08-01 16:31:11 +0800

    更改鼠标点击特效文字

--- js/about.js
+++ js/about.js
@@ -454,7 +454,7 @@ function timer() {
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        var a = new Array("❤郑恩地❤","❤李知恩❤","❤金泰妍❤","❤希望天空❤","❤除了春天、爱情和樱花❤","❤why❤","❤少女的少年❤","❤palette❤","❤rain❤","❤夜信❤","❤blueming❤","❤离别初体验❤");
+        var a = new Array("❤欢迎❤","❤欢迎❤","❤热烈欢迎❤");
         var $i = $("<span></span>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
